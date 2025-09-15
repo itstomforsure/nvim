61e876c75992202ec221748679e7320bdd4956a1
Tom Zavodszky <wzxzje00@gmail.com>
2025-09-15 12:47:39 +0200

    terminal refactor

--- lua/terminal.lua
+++ lua/terminal.lua
@@ -1,77 +1,47 @@
 local vim = vim
+local utils = require("utils")
 local M = {}
 local terminal_buf = nil
 local terminal_win = nil
 
-local function create_terminal_buf()
-	if terminal_buf and vim.api.nvim_buf_is_valid(terminal_buf) then
-		return terminal_buf
-	end
-
-	local buf = vim.api.nvim_create_buf(false, true)
-	vim.bo[buf].buftype = "nofile"
-	vim.bo[buf].bufhidden = "wipe"
-
-	return buf
-end
-
-local function create_terminal_win(buf)
-	if terminal_win and vim.api.nvim_win_is_valid(terminal_win) then
-		return terminal_win
-	end
-
-	local width = math.min(80, vim.o.columns - 10)
-	local height = math.floor(vim.o.lines * 0.4)
-	local row = math.floor((vim.o.lines - height) / 2 - 1)
-	local col = math.floor((vim.o.columns - width) / 2)
-
-	local opts = {
-		relative = "editor",
-		width = width,
-		height = height,
-		row = row,
-		col = col,
-		style = "minimal",
-		border = "rounded",
-	}
-
-	local win = vim.api.nvim_open_win(buf, true, opts)
-	vim.api.nvim_win_set_option(win, "winblend", 10)
-
-	return win
-end
-
-local function close_terminal()
-	if terminal_win and vim.api.nvim_win_is_valid(terminal_win) then
+local function close()
+	if utils.is_win_valid(terminal_win) then
 		vim.api.nvim_win_close(terminal_win, true)
 		terminal_win = nil
 	end
-	if terminal_buf and vim.api.nvim_buf_is_valid(terminal_buf) then
+
+	if utils.is_buf_valid(terminal_buf) then
 		vim.api.nvim_buf_delete(terminal_buf, { force = true })
 		terminal_buf = nil
 	end
+
 	vim.cmd("stopinsert")
 end
 
+local function buf_keybinds()
+	local opts = { buffer = terminal_buf, silent = true }
+	vim.keymap.set({ "n", "t" }, "<Esc>", function()
+		close()
+	end, opts)
+end
+
 local function open_terminal_win()
-	if terminal_win and vim.api.nvim_win_is_valid(terminal_win) then
-		vim.api.nvim_set_current_win(terminal_win)
-	else
-		terminal_buf = create_terminal_buf()
-		terminal_win = create_terminal_win(terminal_buf)
+	terminal_buf = utils.create_scratch_buf(terminal_buf)
+	terminal_win = utils.create_floating_win(terminal_buf, terminal_win)
 
-		vim.cmd("terminal")
-		vim.cmd("startinsert")
+	vim.cmd("terminal")
+	vim.cmd("startinsert")
 
-		local opts = { buffer = terminal_buf, silent = true }
-		vim.keymap.set({ "n", "t" }, "<Esc>", function()
-			close_terminal()
-		end, opts)
-	end
+	buf_keybinds()
 end
 
 function M.setup(key)
-	vim.keymap.set({ "n", "v" }, key, open_terminal_win, { noremap = true, silent = true })
+	local opts = {
+		noremap = true,
+		silent = true,
+		desc = "Open terminal window",
+	}
+	vim.keymap.set({ "n", "v" }, key, open_terminal_win, opts)
 end
 
 return M
